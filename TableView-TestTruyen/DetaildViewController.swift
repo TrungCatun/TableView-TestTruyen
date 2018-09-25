c0155f973aea2f4288fe98e249cf7210768dc0f0
Trung <>
2018-09-25 07:12:19 +0700

    add

--- TableView-TestTruyen/DetaildViewController.swift
+++ TableView-TestTruyen/DetaildViewController.swift
@@ -8,18 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class DetaildViewController: UIViewController {
+    var data: String?
+    
+    @IBOutlet weak var outputTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        if data != nil {
+            outputTextField.text = data
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if outputTextField.text != nil {
+            data = outputTextField.text
+        }
+    }
 
 }
 
